47aa78f3560e31335cea02ecec9d60c2480a8cac
HemaMurthy <50341881+HemaMurthy@users.noreply.github.com>
2019-06-21 00:42:39 -0700

    works - TODO:add technician arrival date in schema

--- ml_push_technician.py
+++ ml_push_technician.py
@@ -30,18 +30,18 @@ def getSQlEngine():
     return engine, meta
     
 #directory where ekryp-data-db-prod tables are stored
-DATADIR = '/home/hema_murthy/testModel/testDataSource' #change to the necessary directory 
+DATADIR = '/home/hema_murthy/testModel/TestDataSource' #change to the necessary directory 
 
 #alter this to match the table structure
 def getTableSchema(tableName, meta):
-    if tableName == 'service_request_history': #need to change it to ml-refernence tables
+    if tableName == technician_history: #need to change it to ml-refernence tables
         return Table(tableName, meta,
                 Column('ekryp_customer_id', Integer, nullable=False),
                 Column('customer_asset_identifier',Integer, nullable=False),
                 Column('asset_serial_number', String(15), nullable=False),
-                Column('service_order_date',String(10), nullable=False),
+                Column('serial_order_date',String(10), nullable=False),
                 Column('service_order_provider', Integer, nullable=False),
-                Column('technician_id', Integer), nullable=False),
+                Column('technician_id', Integer, nullable=False),
                 Column('technician_name', String(30), nullable=False)
         )
 
@@ -57,25 +57,28 @@ sr=panda.read_csv(os.path.join(DATADIR,'service_order_record.csv'))
 products=panda.read_csv(os.path.join(DATADIR,'asset_ib_info.csv'))
 field_tech=panda.read_csv(os.path.join(DATADIR,'field_service_ops_activity_record.csv'))
 
-sr=sr.join(products,on='customer_asset_identifier',how='inner',lsuffix='customer_asset_identifier')
-sr=sr.join(field_service_ops_activity_record,on='field_technician',how='inner',lsuffix='field_rep')
+sr=field_tech.join(sr.set_index('id'),how='inner',on='field_service_order_id',lsuffix='new')
+sr=sr.join(products.set_index('customer_asset_identifier'),on='customer_asset_identifier',lsuffix='new')
+
+#sr=sr.join(products,on='customer_asset_identifier',how='inner',lsuffix='customer_asset_identifier')
 
 #alter this to fit the info in df to match the columns in table
 df = panda.DataFrame({
     'ekryp_customer_id':1,
     'customer_asset_identifier': sr['customer_asset_identifier'],
     'asset_serial_number':sr['serial_number'],
-    'service_order_date': sr['date'],
+    'serial_order_date': sr['service_order_created_on'],
     'service_order_provider':sr['service_provider'],
-    'technician_id': sr['technician_id'], 
-    'technician_name': sr['service_rep']
+    'technician_id':sr['field_technician_id'],
+    'technician_name': sr['field_technician'],
+  #  'technician_arrival_date':sr['date_arrival']
 })
-
+df=df.astype({'technician_id':int})
 #table you want to populate
-tablename='technician_history'
+technician_history='technician_history'
 
 sqlEngine, sqlMeta = getSQlEngine()
-tableSchema= getTableSchema(tablename,sqlMeta)
-pushToSQL(tableSchema,df,'service_request_history',sqlEngine,sqlMeta)
+tableSchema= getTableSchema(technician_history,sqlMeta)
+pushToSQL(tableSchema,df,technician_history,sqlEngine,sqlMeta)
 
 print 'All done'
